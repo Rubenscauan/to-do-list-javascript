484fa626c7839f02c34bc648489860fc182fd86f
Rubens Cauan <rubenscauanfc2021@gmail.com>
2023-03-08 12:07:56 -0300

    submissao css melhorado

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function showEscola() {
     for (let i = 0; i < values.length; i++) {
         lista.innerHTML += `<li>${values[i]['name']}
         <button id='removerEscola' onclick='removeItemEscola("${values[i]['name']}")'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg>
         </button>
         </li>`
@@ -213,6 +213,7 @@ function newTaskPessoal() {
     else if (validaTaskPessoal()) {
         alert("Ja existe uma tarefa com esse nome!");
         input.value = '';
+
     }
 
     else {
